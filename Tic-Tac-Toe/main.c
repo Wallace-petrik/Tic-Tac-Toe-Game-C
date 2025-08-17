c6f9497d469e1d17286146fa60b9bdbf772b603d
Wallace Petrik <116586480+Wallace-petrik@users.noreply.github.com>
2025-08-17 07:07:05 -0300

    Implementação da impressão do tabuleiro

--- Tic-Tac-Toe/main.c
+++ Tic-Tac-Toe/main.c
@@ -2,8 +2,35 @@
 #include <stdlib.h>
 #include <locale.h>
 
+#define tam 3
+
 int main(){
     setlocale(LC_ALL,"");
 
+        char tabuleiro[tam][tam];
+
+        for(int i=0; i < tam; i++){
+            for(int j=0; j < tam; j++){
+                tabuleiro[i][j] = 'x';
+            }
+        }
+
+        printf(" 0     1     2\n");
+        printf("\n");
+        for(int i=0; i < tam; i++){
+            for(int j=0; j < tam; j++){
+                printf(" %c ",tabuleiro[i][j]);
+                if(j<2){
+                    printf(" | ");
+                }
+                if(j==2){
+                    printf("  %d ",i);
+                }
+            }
+            if(i<2){
+                printf("\n--------------\n");
+            }
+        }
+        printf("\n");
     return 0;
 }
